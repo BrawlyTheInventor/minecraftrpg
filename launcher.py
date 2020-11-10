56ffee148c7f2debe55947990cfb7e0732ce0ac1
BrawlyTheInventor <49785023+BrawlyTheInventor@users.noreply.github.com>
2020-11-09 21:25:29 -0500

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -1,16 +1,23 @@
-update = input("Update & Play (y) or Play (n)?")
-if update == "y":
-    import requests
-    #https://raw.githubusercontent.com/BrawlyTheInventor/minecraftrpg/client/mcmultiplayer.py
-    import requests
+print("Attempting to update...")
+print("If update fails, go to mcsingleplayer.py, and F5")
+import requests
+#https://raw.githubusercontent.com/BrawlyTheInventor/minecraftrpg/client/mcmultiplayer.py
 
-    x = requests.get('https://raw.githubusercontent.com/BrawlyTheInventor/minecraftrpg/client/mcmultiplayer.py')
+x = requests.get('https://raw.githubusercontent.com/BrawlyTheInventor/minecraftrpg/client/mcmultiplayer.py')
 
-    #print(x.text)
+with open('mcmultiplayer.py', 'w') as multiupdate:
+    multiupdate.write(x.text)
 
-    with open('mcmultiplayer.py', 'w') as app:
-      app.write(x.text)
+
+xsi = requests.get('https://raw.githubusercontent.com/BrawlyTheInventor/minecraftrpg/client/mcsingleplayer.py')
+
+with open('mcsingleplayer.py', 'w') as singleupdate:
+    singleupdate.write(xsi.text)
+
+with open('mcmultiplayer.py', 'w') as multiupdate:
+    multiupdate.write(x.text)
+version = input("Multiplayer (m) or Singleplayer (s)?")
+if version == "m":
     from mcmultiplayer import *
-
-elif update == "n":
-    from mcmultiplayer import *
+elif version == "s":
+    from mcsingleplayer import *
